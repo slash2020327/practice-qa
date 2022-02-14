4b74ddfeff6fcd0850ecbfb651c4b72f39156617
kschernopiatova <kchernopiatova@solvd.com>
2022-02-14 16:35:27 +0300

    getDriver method was corrected

--- src/main/java/com/solvd/practiceqa/web/util/DriverUtil.java
+++ src/main/java/com/solvd/practiceqa/web/util/DriverUtil.java
@@ -1,14 +1,22 @@
 package com.solvd.practiceqa.web.util;
 
+import com.solvd.practiceqa.web.service.ConfigData;
+import com.solvd.practiceqa.web.service.ConfigService;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.lang.invoke.MethodHandles;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class DriverUtil {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
     public static ConcurrentHashMap<String, WebDriver> drivers = new ConcurrentHashMap<>();
 
     public static WebDriver getDriver(String name) {
@@ -19,14 +27,14 @@ public class DriverUtil {
             }
         }
         if (driver == null) {
-            driver = new ChromeDriver();
+            driver = driverInit();
             drivers.put(name, driver);
         }
         return driver;
     }
 
     public static WebDriver getOptionsDriver(String name, ChromeOptions options) {
-        ChromeDriver driver = new ChromeDriver(options);
+        WebDriver driver = new ChromeDriver(options);
         drivers.put(name, driver);
         return driver;
     }
@@ -45,5 +53,25 @@ public class DriverUtil {
             entry.getValue().quit();
             drivers.remove(entry.getKey());
         }
+    }
+
+    public static WebDriver driverInit() {
+        String browser = ConfigService.getValue(ConfigData.BROWSER);
+        WebDriver driver = null;
+        switch (browser) {
+        case "chrome":
+            driver = new ChromeDriver();
+            break;
+        case "safari":
+            driver = new SafariDriver();
+            break;
+        case "firefox":
+            driver = new FirefoxDriver();
+        default:
+            LOGGER.warn("Browser is not defined");
+        }
+        assert driver != null;
+        driver.manage().window().maximize();
+        return driver;
     }
 }
