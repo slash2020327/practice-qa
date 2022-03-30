f1e395ec8e8284bc00b2dcc977129ca076e27e72
kschernopiatova <98829151+kschernopiatova@users.noreply.github.com>
2022-03-30 17:51:03 +0300

    Listeners were added

--- src/test/java/com/solvd/practiceqa/AbstractTest.java
+++ src/test/java/com/solvd/practiceqa/AbstractTest.java
@@ -1,15 +1,28 @@
 package com.solvd.practiceqa;
 
+import com.nordstrom.automation.testng.LinkedListeners;
 import com.qaprosoft.carina.core.foundation.IAbstractTest;
+import com.qaprosoft.carina.core.foundation.listeners.CarinaListener;
+import com.qaprosoft.carina.core.foundation.listeners.CarinaListenerChain;
+import com.qaprosoft.carina.core.foundation.listeners.FilterTestsListener;
 import com.solvd.practiceqa.web.service.ConfigService;
 import com.solvd.practiceqa.web.service.TestDataService;
-import org.testng.annotations.*;
+import com.zebrunner.agent.testng.listener.TestRunListener;
+import org.testng.annotations.BeforeSuite;
+import org.testng.internal.annotations.ListenersAnnotation;
 
+@LinkedListeners({ CarinaListener.class, TestRunListener.class, FilterTestsListener.class })
 public abstract class AbstractTest implements IAbstractTest {
+
+    protected ListenersAnnotation listenersAnnotation;
+    protected CarinaListenerChain listener;
 
     @BeforeSuite
     public void beforeSuite() {
         ConfigService.createInstance();
         TestDataService.createInstance();
+        listenersAnnotation = new ListenersAnnotation();
+        listener = new CarinaListenerChain();
+        listener.transform(listenersAnnotation, AbstractTest.class);
     }
 }
